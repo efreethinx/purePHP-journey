284d929f2d3bfa6a5d595626cc318e3bf79618eb
Nursandi <sandinur157@gmail.com>
2022-02-10 11:09:59 +0700

    007

--- index.php
+++ index.php
@@ -1,37 +1,62 @@
 <?php
 session_start();
-error_reporting(0);
+error_reporting(1);
 
 // Ambil data user dari file
 $file_handle = fopen("config.txt", "rb");
-    $line_of_text = fgets($file_handle);
-    $parts = explode('|', $line_of_text);
-    $user = $parts[0];
-    $pass = $parts[1];
+// $line_of_text = fgets($file_handle);
+// $parts = explode('|', $line_of_text);
+// $user = $parts[0];
+// $pass = $parts[1];
+
+$rows = [];
+while($item = fgets($file_handle)) {
+    list( $word ) = explode( "++", $item );
+
+    $words = explode('|', $word);
+
+    $row = [];
+    $row['nik'] = rtrim($words[0], ' ');
+    $row['password'] = rtrim($words[1], ' ');
+    $row['email'] = rtrim($words[2], "\n");
+
+    array_push($rows, $row);
+}
+
 fclose($file_handle);
 
+$loginIsInvalid = false;
 if (isset($_POST['submit'])) {
-    if ($_POST['username'] == $user && $_POST['password'] == $pass) {
+    $loginIsValid = array_filter($rows, function ($item) {
+        if ($_POST['username'] == $item['nik'] && $_POST['password'] == $item['password']) {
+            return $item;
+        }
+    });
+
+    if ($loginIsValid) {
         //Membuat Session
         $_SESSION["username"] = $_POST['username'];
         $_SESSION["password"] = $_POST['password'];
+
         /*Jika Ingin Pindah Ke Halaman Lain*/
         header("Location: home.php"); //Pindahkan Kehalaman Admin
+        exit;
     } else {
-        // Tampilkan Pesan Error
-        display_login_form();
-        echo '<p align="center" style="color:red;">Username Atau Password Tidak Benar</p>';
+        $loginIsInvalid = true;
     }
-} else {
-    display_login_form();
 }
-function display_login_form() { ?>
-    <form align="center" action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
-        <h1>Login</h1>
-        <input size="30" type="text" name="username" id="username" value="NIK" required>
-        <br><br>
-        <input size="30" type="text" name="password" id="password" value="Nama Lengkap" required>
-        <br><br>
-        <a href="page/registrasi_form.php"><button type="button">Saya Pengguna Baru</button></a> <button type="submit" class="btn" name="submit" value="submit">Masuk</button>
-    </form>
-<?php } ?>
+
+?>
+
+<form align="center" action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+    <h1>Login</h1>
+    <input size="30" type="text" name="username" id="username" placeholder="NIK" required>
+    <br><br>
+    <input size="30" type="text" name="password" id="password" placeholder="Nama Lengkap" required>
+    <br><br>
+    <a href="page/registrasi_form.php"><button type="button">Saya Pengguna Baru</button></a> <button type="submit" class="btn" name="submit" value="submit">Masuk</button>
+
+    <?php if ($loginIsInvalid) : ?>
+    <p align="center" style="color:red;">Username Atau Password Tidak Benar</p>
+    <?php endif; ?>
+</form>
